ed85973056b8d9aeaba6d6688941a0d3ea858cb4
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-04-23 14:28:48 +0200

    create routes for each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import Artifacts from './Pages/Artifacts'
+import Essence from './Pages/Essence'
+import Home from './Pages/Home'
+import ScrumTeam from './Pages/ScrumTeam'
+import SprintEvents from './Pages/SprintEvents'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='essence' element={<Essence />}/>
+        <Route path='artifacts' element={<Artifacts />}/>
+        <Route path='scrum-team' element={<ScrumTeam />}/>
+        <Route path='events' element={<SprintEvents />}/>
+      </Routes>
     </>
   )
 }
