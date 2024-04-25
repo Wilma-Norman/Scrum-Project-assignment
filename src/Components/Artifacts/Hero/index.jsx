1e3fd410877c4025e67fcbca062498d38124d51d
Memai2023 <maria.hong26@gmail.com>
2024-04-26 00:23:27 +0200

    modified and added media screen

--- src/Components/Artifacts/Hero/index.jsx
+++ src/Components/Artifacts/Hero/index.jsx
@@ -4,7 +4,7 @@ import styles from './Artifacts.module.css'
 const ArtifactHero = () => {
     return (
         <div className={styles.heroContainer}>
-            <h1 className={styles.header}>The three artifacts</h1>
+            <h1 className={styles.header}>Three Artifacts <br/> of Scrum</h1>
             <hr className={styles.heroLine}></hr>
         </div>
     )
