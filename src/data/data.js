2d56b562e1432f816c52bdad48024a8a1767b92c
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-04-25 15:36:42 +0200

    create objects for home and essence

---
+++ src/data/data.js
@@ -0,0 +1,56 @@
+export const Home = {
+    heroTitle: 'Welcometo The Scrum Guide',
+    aboutText: 'We designed The Scrum Guide website to help people understand what Scrum is, how it works and why it is a popular and amazing agile project management framework to use!',
+    readyToGoText: "Ready to give it a shot? We promise we won't let you down!",
+    heroImgSource: '/Images/home.jpg'
+}
+
+export const Essence = {
+    heroTitle: "The Essence of Scrum",
+    whatText: "It is an agile project management framework. Imagine Scrum like breaking work into small steps that you do as a team, trying new things as you go and getting feedback to get better. It's about working together to deliver value bit by bit. Scrum gives you a bit of a roadmap to follow, but you can customize it to fit what works best for you.",
+    whyText: "Scrum draws its name from the Rugby formation, in which players huddle together in a 'scrum,' aiming to move the ball forward. Translating this concept to a development team, Scrum unites the team with the common objective of progressing the product.",
+    historyText: "Scrum is a framework defined in 1995 by the creators Jeff Sutherland and Ken Schwaber. If you need to understand how Scrum works, but don't want to read the founder's well written and comprehensive Scrum Guide, you are in the right place! A Scrum team, consisting of a Product Owner, a Scrum Master, and Developers, is formed with specific accountabilities within the framework of Scrum methodology. Simply put;",
+    dynamicsText: "In Scrum, a Scrum Master creates a supportive atmosphere where:",
+    bulletOne: "The Product Owner organizes tasks for a complex issue in a Product Backlog.",
+    bulletTwo: "The Scrum Team tackles chosen tasks to create valuable progress during a Sprint.",
+    bulletThree: "The Scrum Team and stakeholders review the outcomes and adapt for the next Sprint",
+    bulletFour: "This cycle repeats.",
+    theoryText: "Since we know that scrum is a practical agile framework, why should we care about the theory? The answer is simple, scrum is like a guidebook. It shows us what’s important, sets rules and tells us how to apply them to reach our goals and achieve the things we value. Understanding its philosophy and principles is essential to follow its guidance.",
+    secondTheoryText: "Scrum is founded on empiricism, which means that knowledge comes from experience and decisions are made from what is observed and from experimentation. It is also founded on Lean thinking to reduce waste and focuses on the essentials.",
+    empiricismText: "Framework of development needs pillars to be supported  in the concept of working iteratively and to make it resistant to adverse effects. For Scrum these pillars are:",
+    inspectionText: "To review the progress and variances, the team should frequently inspect Scrum artifacts, such as the product and sprint backlog.",
+    inspectionPS: "We keep checking what we’ve done.",
+    adaptionText:"If there are variations from the inspection that must be changed the team should be able to learn from this and make adjustments to improve it.",
+    adaptionPS: "If something we’re doing doesn’t work well, we change it.",
+    transparencyText: "To expose to the team our way of working, the way we think and make it visible for everyone who is involved to reduce risks and decisions that can lead to diminish values.",
+    transparencyPS: "Everyone knows what we’re doing.",
+    fiveValuesText: "To make the best and successful use of Scrum there are five values that also need to be met by everyone in the Team. The values help the team understand how to apply the Scrum rules and ceremonies in a good way and think of them as communication opportunities and improvement through making progress. The five values are:",
+    commitmentCircleText:"The team should commit themselves and the goal. To help each other achieve the goal and finish tasks.",
+    courageCircleText: "To be willing to accept when they are wrong, do the right things and work through difficult conversations and problems. To admit their mistakes.",
+    focusCirleText:"Finish what they started and focus on accomplishing the goal and deliver the product.",
+    openessCircleText:"To be honest and open to the team and its shareholders in what they do and deal with any challenges when performing that work.",
+    respectCircleText:"Treat each other with respect and respect each other's backgrounds, experiences of work and abilities.",
+    houseOfScrumTextOne: "Think of Scrum like a house. The foundation of this house is empiricism, and the pillars holding it up are Adaptation, Inspection, and Transparency. The walls are built with bricks representing the Scrum values. This analogy helps us understand how Scrum works.",
+    houseOfScrumTextTwo:"Just like a house, if you remove the foundation, the house falls. If the pillars break, the roof caves in. If the walls get damaged, the house is exposed to weather. This metaphor shows how crucial it is to have a solid structure in Scrum, with all its parts working together.",
+    heroImgSource: "/Images/greenstrokes.jpg",
+    frameworkImgSource:"/Images/scrum-overview.jpg",
+    historyDynamicsImgSource:"/Images/green2.jpg",
+    manWorkingImgSource: "/Images/manworking.jpg"
+}
+
+export const ScrumTeam = {
+    heroTitle: "The Scrum Team",
+    whatText: "In a Scrum team, individuals collaborate closely to achieve the project's goals efficiently and effectively.",
+    keyRolesText: "Key roles include:",
+    bulletOne: "The Product Owner (PO)",
+    bulletTwo: "The Scrum Master",
+    bulletThree: "The Development Team",
+    productOwnerText: "Represents the stakeholders and prioritizes the backlog. This person serves as the liaison between stakeholders, which may include customers, users, and the business. Their primary responsibility is to curate and prioritize the product backlog, ensuring that the team focuses on delivering the most valuable items first. The Product Owner works closely with the team to refine requirements and validate completed work.",
+    scrumMasterText: "Serves as a guiding force for the team, ensuring adherence to Scrum principles and practices. Acting as a servant-leader, they facilitate the Scrum process, remove obstacles hindering progress, and coach the team in self-organization and cross-functionality. Additionally, the Scrum Master fosters a culture of continuous improvement by helping team members understand and embrace Scrum principles and practices.",
+    developmentTeamText:"Consists of skilled professionals tasked with delivering potentially shippable product increments at the end of each Sprint. Composed of cross-functional members, the team autonomously organizes and executes their work. They collaborate closely with the Product Owner to comprehend requirements and ensure the delivery of valuable solutions to the customer.",
+    conclusionText: "Each role brings unique skills and perspectives, fostering a dynamic and self-organizing environment where innovation thrives and value is consistently delivered.",
+    heroImgSource: "/Images/purplefront.jpg",
+    teamImageSource: "/Images/teamwork.jpg",
+    circleImageSource: "/Images/ScrumRoles.png"
+}
+
