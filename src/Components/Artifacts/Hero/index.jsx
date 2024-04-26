96ca707320b109bf7c7bb580af231021f202fcdd
Memai2023 <maria.hong26@gmail.com>
2024-04-26 22:18:22 +0200

    added dynamic backgroundimage and h1 from data.js

--- src/Components/Artifacts/Hero/index.jsx
+++ src/Components/Artifacts/Hero/index.jsx
@@ -1,9 +1,10 @@
 import styles from './Artifacts.module.css'
+import { Artifacts } from '../../../data/data'
 
 const ArtifactHero = () => {
     return (
-        <div className={styles.heroContainer}>
-            <h1 className={styles.header}>THREE ARTIFACTS <br/> OF SCRUM</h1>
+        <div style={{backgroundImage:`url(${Artifacts.heroImgSource})`}} className={styles.heroContainer}>
+            <h1 className={styles.header}>{Artifacts.heroTitle}</h1>
             <hr className={styles.heroLine}></hr>
         </div>
     )
