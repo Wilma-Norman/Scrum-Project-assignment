a8af224dbb9a4be03121e6a61209d054635ae044
Memai2023 <maria.hong26@gmail.com>
2024-04-26 11:33:06 +0200

    scrum-37, adjusted code according to the codereview and changed order in alphabetic order

--- src/Components/Artifacts/Hero/index.jsx
+++ src/Components/Artifacts/Hero/index.jsx
@@ -1,10 +1,9 @@
 import styles from './Artifacts.module.css'
 
-
 const ArtifactHero = () => {
     return (
         <div className={styles.heroContainer}>
-            <h1 className={styles.header}>Three Artifacts <br/> of Scrum</h1>
+            <h1 className={styles.header}>THREE ARTIFACTS <br/> OF SCRUM</h1>
             <hr className={styles.heroLine}></hr>
         </div>
     )
