bd03ad048f6c7e43199f43659f298610102b8c9f
Memai2023 <maria.hong26@gmail.com>
2024-04-29 16:23:06 +0200

    changed to correct info to be displayed

--- src/Pages/Artifacts/index.jsx
+++ src/Pages/Artifacts/index.jsx
@@ -1,9 +1,10 @@
 import Produces from "../../Components/Artifacts/Produces"
+
 const Artifacts = () => {
     return(
-        <div>
+        <>
             <Produces/>
-        </div>
+        </>
     )
 }
 export default Artifacts
