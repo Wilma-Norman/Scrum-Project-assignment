275181908f1ec1034ccbf4e856329de64e5ae2c0
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-04-29 12:19:06 +0200

    add image, center title

--- src/Components/Essence/History/index.jsx
+++ src/Components/Essence/History/index.jsx
@@ -1,12 +1,16 @@
-import Card from '../../Card'
 import { Essence } from '../../../data/data'
 import styles from './History.module.css'
 
 const History = () => {
     return(
-        <div className={styles.cardContainer}>
-            <Card title={'A Brief History & The Dynamics'} text={null}/>
-            <Card title={null} text={Essence.historyText}/>
+        <div className={styles.historyContainer}>
+            <div className={styles.contentContainer}>
+                <div className={styles.titleContainer}>
+                    <h2>A Short History<br/> & <br/>The Dynamics</h2>
+                </div>
+                <p>{Essence.historyText}</p> 
+            </div>
+            <img src={Essence.frameworkImgSource} alt='Overview of The Scrum Framework'/>
         </div>
     )
 }
