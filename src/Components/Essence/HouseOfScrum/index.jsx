bd1483031c13a955641a07e4284d9bf2f80f114f
Lisette Svan <svanlisette@gmail.com>
2024-04-29 14:35:55 +0200

    import text from data instead of hard code

--- src/Components/Essence/HouseOfScrum/index.jsx
+++ src/Components/Essence/HouseOfScrum/index.jsx
@@ -1,4 +1,5 @@
 import styles from './HouseOfScrum.module.css'
+import { Essence } from '../../../data/data'
 
 const HouseOfScrum = () => {
     return (
@@ -7,8 +8,8 @@ const HouseOfScrum = () => {
             <img src="../Public/Images/greenhouse.jpg" alt="house of scrum picture" />
             <div className={styles.text}>
                 <h1 className={styles.title}>The house of Scrum</h1>
-                <p>Think of Scrum like a house. The foundation of this house is empiricism, and the pillars holding it up are Adaptation, Inspection, and Transparency. The walls are built with bricks representing the Scrum values. This analogy helps us understand how Scrum works.</p>
-                <p>Just like a house, if you remove the foundation, the house falls. If the pillars break, the roof caves in. If the walls get damaged, the house is exposed to weather. This metaphor shows how crucial it is to have a solid structure in Scrum, with all its parts working together.</p>
+                <p>{Essence.houseOfScrumTextOne}</p>
+                <p>{Essence.houseOfScrumTextTwo}</p>
             </div>
         </div>
         </>
