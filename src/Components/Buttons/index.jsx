8da81094fa8dc9e52a27279895c3ef0960c5d5ce
Memai2023 <maria.hong26@gmail.com>
2024-04-30 23:14:43 +0200

    adjusted code

--- src/Components/Buttons/index.jsx
+++ src/Components/Buttons/index.jsx
@@ -1,20 +1,18 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Link } from 'react-router-dom'
-const Buttons = ({direction, path}) => {
-  
+const Buttons = ({direction, path}) => {  
     return (
         <>
-          {direction === 'forward' ? (
-            <Link to={path}>
-              <ChevronRight />
-            </Link>
-          ) : (
-            <Link to={path}>
-              <ChevronLeft />
-            </Link>
-          )}
+            {direction === "forward" ? (
+                <Link to={path}>
+                    <ChevronRight/>
+                </Link>
+            ):(
+                <Link to={path}>
+                    <ChevronLeft/>
+                </Link>
+            )}
         </>
-      )   
+    )
 }
-
 export default Buttons
