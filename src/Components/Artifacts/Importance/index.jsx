af1e647f4f0462ec6b4e3c87198134ec287d6c7e
Memai2023 <maria.hong26@gmail.com>
2024-05-01 08:16:59 +0200

    adjusted css

--- src/Components/Artifacts/Importance/index.jsx
+++ src/Components/Artifacts/Importance/index.jsx
@@ -2,16 +2,19 @@ import styles from './Importance.module.css'
 import { Artifacts } from '../../../data/data'
 import Card from '../../Card'
 import { Link } from 'react-router-dom'
+import Buttons from '../../Buttons'
 
 const Importance = () => {
     return (
         <div className={styles.importanceContainer}>
-            <div>
+            <div className={styles.imageWrapper}>
                 <img src={Artifacts.teamCelebratingImgSource} alt="Team"/>
             </div>
             <div>
                 <Card title="The Importance of the Artifacts" text={Artifacts.importanceOfArtifactsText}/>
-                <Link to="/events">Link</Link>
+                <div className={styles.navigationButton}>
+                    <Buttons direction="back" path="/events"/>
+                </div>
             </div>
         </div>
     )
