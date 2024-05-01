f1f5630b17da9d28b92744a2db2e5a92d27c51f7
Memai2023 <maria.hong26@gmail.com>
2024-05-01 10:02:12 +0200

    created new button component

---
+++ src/Components/Buttons/index.jsx
@@ -0,0 +1,19 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Link } from 'react-router-dom'
+
+const Buttons = ({direction, path}) => {  
+    return (
+        <>
+            {direction === "forward" ? (
+                <Link to={path}>
+                    <ChevronRight/>
+                </Link>
+            ):(
+                <Link to={path}>
+                    <ChevronLeft/>
+                </Link>
+            )}
+        </>
+    )
+}
+export default Buttons
