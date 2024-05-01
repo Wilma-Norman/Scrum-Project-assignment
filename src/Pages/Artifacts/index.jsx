ad26f454f08e093a4cb84fc4a7d5fdb8895a9f85
Memai2023 <maria.hong26@gmail.com>
2024-05-01 15:01:48 +0200

    erased css under pages and restored index.jsx

--- src/Pages/Artifacts/index.jsx
+++ src/Pages/Artifacts/index.jsx
@@ -1,14 +1,12 @@
 import Importance from "../../Components/Artifacts/Importance"
-import styles from "./Artifacts.module.css"
-
 
 const Artifacts = () => {
     return(
-        <div className={styles.artifactContainer}>
+        <>
 
             <Importance/>
 
-        </div>
+        </>
     )
 }
 
