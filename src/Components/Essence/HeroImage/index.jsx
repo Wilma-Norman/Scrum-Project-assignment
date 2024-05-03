b2dd271f4fd4a96653d15dd85d088ba6f4d46376
Lisette Svan <svanlisette@gmail.com>
2024-05-03 20:29:56 +0200

    made hero page for essence page

--- src/Components/Essence/HeroImage/index.jsx
+++ src/Components/Essence/HeroImage/index.jsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import styles from './HeroImage.module.css';
+
 const HeroImage = () => {
     return (
-        <img src="" alt="" />
-    )
-}
+        <div className={styles.hero}>
+            <img src="../../Public/Images/greenstrokes.jpg" alt="green picture" className={styles.image} />
+            <div className={styles.overlay}>
+                <h1 className={styles.text}>The Essence of Scrum</h1>
+            </div>
+        </div>
+    );
+};
+
+export default HeroImage;
